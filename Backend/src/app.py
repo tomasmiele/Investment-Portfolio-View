25ff6a8530d7cb1e82d7e716bd233eeabb6d4b4b
Tomas Miele <tomasrm@al.insper.edu.br>
2024-10-09 15:21:12 -0300

    feat: backend setup

---
+++ Backend/src/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API de Gerenciamento de Eventos",
+    description="API para gerenciar eventos, locais e tipos de eventos.",
+    version="1.0.0"
+)
